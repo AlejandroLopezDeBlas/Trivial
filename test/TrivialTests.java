2a2bbd62d50821eb6c974d890d742038a529265a
AlejandroLopezDeBlas <alejandro.lopezdblas@iesfm.onmicrosoft.com>
2022-02-18 11:08:24 +0100

    Eliminamos códigorepetido de posición de jugador

--- test/TrivialTests.java
+++ test/TrivialTests.java
@@ -12,4 +12,23 @@ public class TrivialTests {
     public void crear_Game(){
         Game trivial = new Game();
     }
+
+    @Test
+    public void si_al_prinipio_saco_un_1_voy_a_casilla_1(){
+        //Arrange
+        Game sut = new Game();
+        sut.agregar("Maria");
+        sut.agregar("Juan");
+
+        sut.tirarDado(1);
+
+        String expected = "La nueva posicion de Maria es 1";
+
+        //Act
+
+        String actual = sut.nuevaPosicionJugador();
+
+        //Assert
+        Assertions.assertEquals(expected,actual);
+    }
 }
