01657e26f48116b70ff22027d980598dcc731367
AlejandroLopezDeBlas <alejandro.lopezdblas@iesfm.onmicrosoft.com>
2022-02-23 10:01:13 +0100

    Explicación Problema 2

--- test/TrivialTests.java
+++ test/TrivialTests.java
@@ -17,7 +17,7 @@ public class TrivialTests {
     public void si_al_prinipio_saco_un_1_voy_a_casilla_1(){
         //Arrange
         Game sut = new Game();
-        sut.agregar("Maria");
+        sut.agregar("María");
         sut.agregar("Juan");
         sut.agregar("Maria");
         sut.agregar("Juan");
